da841bb39ea1151fb7feb8aaaa00fda679f8eed4
Justin Shaw <easyworkemail@gmail.com>
2019-04-28 21:07:05 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -28,5 +28,5 @@ def won?(board)  # return false if no winning combo; return winning combination 
   end
 end
 
-board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+board = ["O", "X", "X", " ", " ", " ", " ", " ", " "]
 won?(board)
