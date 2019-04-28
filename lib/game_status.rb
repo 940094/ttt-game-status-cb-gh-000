eb1efe2a4f37d09cb84cfc3dfbc45bf67473833e
Justin Shaw <easyworkemail@gmail.com>
2019-04-28 21:04:09 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,9 +17,12 @@ WIN_COMBINATIONS = [  # 3 vertical, 3 horizontal, 2 diagnol
 
 def won?(board)  # return false if no winning combo; return winning combination index if there is a win
   WIN_COMBINATIONS.each do |win_combinations| # for each winning_combination
-     winning_combination = []
-     win_combinations.all? do |index|  # look at each index in the combination. if each index is an X or O, set the win
-       puts board[index] == 'X'
+     index_0 = win_combinations[0]  # index 0
+     index_1 = win_combinations[1]  # index 0
+     index_2 = win_combinations[2]  # index 0
+     # look at 0, 1, 2 index and if all are X, then there's a win, or if all are O there's a win
+     if (index_0 == 'X' && index_1 == 'X' && index_2 == 'X') || (index_0 == 'O' && index_1 == 'O' && index_2 == 'O')
+       puts win_combinations
      end
   end
 end
