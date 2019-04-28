452cdfe446b012343aeb2bb2c8d2bcb46cd3e10f
Justin Shaw <easyworkemail@gmail.com>
2019-04-28 20:54:55 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,8 +18,8 @@ WIN_COMBINATIONS = [  # 3 vertical, 3 horizontal, 2 diagnol
 def won?(board)  # return false if no winning combo; return winning combination index if there is a win
   WIN_COMBINATIONS.each do |win_combinations| # for each winning_combination
      winning_combination = []
-     puts win_combinations.all? do |index|  # look at each index in the combination. if each index is an X or O, set the win
-       index.even?
+     win_combinations.all? do |index|  # look at each index in the combination. if each index is an X or O, set the win
+       index.include?(String)
      end
   end
 end
